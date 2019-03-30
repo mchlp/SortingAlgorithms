92ed4d86fa4dbfb3f2673014d497cc484e6bddeb
Michael Pu <michael.pu123@gmail.com>
2019-03-29 22:29:50 -0400

    Add more comments

--- src/IntegerSorter.java
+++ src/IntegerSorter.java
@@ -5,28 +5,33 @@
  *  Course: ICS4U
  */
 
+import java.util.Arrays;
+
 /**
  * IntegerSorter class with three different sorting methods.
+ * It also has a timeout setting which will stop the sort when the timeout is passed for the first two sorting methods.
  */
-
-import java.util.Arrays;
 
 public class IntegerSorter implements Sorter {
 
+    // Time in nanoseconds to be passed before a sort method is stopped.
     public static final long KILL_TIME = 1_000_000_000L * 60L * 5L;
+
+    // Number of iterations that should be passed in the sorting method before the time elapsed is checked
+    // (checking elapsed time every loop can take quite a bit of time, which may distort the results)
     public static final long CHECK_INTERVAL_COUNT = 10_000_000_000L;
 
     private int[] list;
 
+    // Sort method 1
     private int[] sort1() {
         long startTime = System.nanoTime();
         long itercount = 0;
         for (int end = list.length - 1; end > 0; end--) {
             for (int i = 0; i < end; i++) {
                 itercount++;
-
                 if (itercount > CHECK_INTERVAL_COUNT) {
-                    itercount=0;
+                    itercount = 0;
                     if (System.nanoTime() - startTime > KILL_TIME) {
                         System.out.println("Killed.");
                         return list;
@@ -42,14 +47,16 @@ public class IntegerSorter implements Sorter {
         return list;
     }
 
+    // Sort method 2
     private int[] sort2() {
         long startTime = System.nanoTime();
-        long itercount = 0;        for (int start = 0; start < list.length - 1; start++) {
+        long itercount = 0;
+        for (int start = 0; start < list.length - 1; start++) {
             for (int i = start + 1; i < list.length; i++) {
                 itercount++;
 
                 if (itercount > CHECK_INTERVAL_COUNT) {
-                    itercount=0;
+                    itercount = 0;
                     if (System.nanoTime() - startTime > KILL_TIME) {
                         System.out.println("Killed.");
                         return list;
@@ -65,6 +72,7 @@ public class IntegerSorter implements Sorter {
         return list;
     }
 
+    // Combines two sorted arrays into one sorted array in place (for sort method 3)
     private void combine(int arr1Start, int arr1End, int arr2Start, int arr2End) {
 
         int[] combinedArr = new int[arr2End - arr1Start];
@@ -92,6 +100,7 @@ public class IntegerSorter implements Sorter {
         System.arraycopy(combinedArr, 0, list, arr1Start, arr2End - arr1Start);
     }
 
+    // Sort method 3
     private void sort3(int[] bounds) {
         if (bounds[1] - bounds[0] > 1) {
             int[] arr1Bounds = {bounds[0], (bounds[1] + bounds[0]) / 2};
--- src/SortTester.java
+++ src/SortTester.java
@@ -13,38 +13,61 @@ import java.util.Random;
 
 public class SortTester {
 
+    // Sizes of the arrays to run benchmark tests on
     public static final int[] ARRAY_SIZES = {16, 128, 1024, 16384, 131072, 1048576, 16777216};
+
+    // Number of sorting methods
     public static final int NUM_SORT_METHODS = 3;
+
+    // Number of trials to run each sort method with each array size
     public static final int NUM_TRIALS = 3;
 
     public static void main(String[] args) {
         IntegerSorter integerSorter = new IntegerSorter();
         Random random = new Random();
 
+        // loops through all the array sizes
         for (int arraySize : ARRAY_SIZES) {
             System.out.println("===== SIZE " + arraySize + " =====");
+
+            // generates array and fills it with "arraySize" number of random integers
             int arr[] = new int[arraySize];
             for (int i=0; i<arraySize; i++) {
                 arr[i] = random.nextInt();
             }
 
+            // creates 2D array to store results from trials
             long[][] resultTimes = new long[NUM_SORT_METHODS][NUM_TRIALS];
+
+            // loops through trials
             for (int trial=1; trial<=NUM_TRIALS; trial++) {
                 System.out.println("--- Trial " + trial + " ---");
 
                 String[] results = new String[NUM_SORT_METHODS];
+
+                // loops through sort methods
                 for (int i = 1; i <= NUM_SORT_METHODS; i++) {
+                    // copies test array to be passed into sorter
                     int[] arrCopy = arr.clone();
                     integerSorter.setList(arrCopy);
+
+                    // start timing sort
                     long startTime = System.nanoTime();
+
                     integerSorter.sort(i);
+
+                    // stop timing sort
                     long endTime = System.nanoTime();
+
+                    // calculate elapsed time and store result
                     long elapsedTime = endTime - startTime;
                     resultTimes[i-1][trial-1] = elapsedTime;
                     System.out.println("Method " + i + ": " + elapsedTime + " nano sec");
+                    System.out.print(integerSorter);
                     results[i - 1] = integerSorter.toString();
                 }
 
+                // check if all sort methods output the same result
                 boolean allSame = true;
                 for (int i = 1; i < NUM_SORT_METHODS; i++) {
                     if (!results[i - 1].equals(results[i])) {
@@ -55,9 +78,11 @@ public class SortTester {
                 if (allSame) {
                     System.out.println("Size " + arraySize + ": All " + NUM_SORT_METHODS + " sort methods returned the same result.");
                 } else {
-                    System.out.println("Size " + arraySize + ": Error: All " + NUM_SORT_METHODS + " sort methods did not return the same result.");
+                    System.out.println("Size " + arraySize + ": Error: All " + NUM_SORT_METHODS + " sort methods did not return the same result. One or more sort methods may have been killed before completion.");
                 }
             }
+
+            // calculates and outputs average of results
             System.out.println("--- Average ---");
             for (int i=0; i<NUM_SORT_METHODS; i++) {
                 long sum = 0;
